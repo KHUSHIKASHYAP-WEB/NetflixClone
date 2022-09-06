1743b7aebe3c3e2d6a05d5abdd50b4b800c4bde5
Khushi Kashyap <khushikashyap126@gmail.com>
2022-09-06 18:33:00 +0530

    firsycommit

--- homescript.js
+++ homescript.js
@@ -1,9 +1,17 @@
-modeChange = document.getElementsByClassName(".changemode");
-// function changeMode() {
-//   console.log("hi im breing called");
-//   modeChange.classList.toggle(".mydark");
-// }
 const changeMode = () => {
-  let mybody = document.body;
-  mybody.classList.toggle("mydark");
+  let main = document.getElementsByClassName("main");
+  let mymain = document.getElementById("main").classList;
+  let myDownload = document.getElementById("Download").classList;
+  let myfaq = document.getElementById("faqs").classList;
+  let mychildren = document.getElementById("childrenProfile").classList;
+  let mywatch = document.getElementById("watchEverywhere").classList;
+  let myontv = document.getElementById("OnTv").classList;
+  let footer = document.getElementById("footer").classList;
+  mymain.toggle("mainlight");
+  myDownload.toggle("mydark");
+  myfaq.toggle("mydark");
+  mychildren.toggle("mydark");
+  myontv.toggle("mydark");
+  mywatch.toggle("mydark");
+  footer.toggle("mydark");
 };
